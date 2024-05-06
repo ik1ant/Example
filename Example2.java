4abec2925c3b84948b319cfa757b1e57881cc452
ik1ant <124435987+ik1ant@users.noreply.github.com>
2024-05-06 22:25:23 +0300

    Chapter 1

---
+++ Example2.java
@@ -0,0 +1,14 @@
+public class Example2 {
+    public static void main(String[] args) {
+        int myVar1, myVar2;
+        float floatNumber = 18.3333333333f;
+        double doubleNumber = 18.3333333333;
+        System.out.println(floatNumber);
+        System.out.println(doubleNumber);
+        myVar1 = 1024;
+        System.out.println("myVar1 содержит " + myVar1);
+        myVar2 = myVar1 / 2;
+        System.out.print("myVar2 содержит myVar1 / 2 ");
+        System.out.println(myVar2);
+    }
+}
---
+++ Example3.java
@@ -0,0 +1,20 @@
+public class Example3 {
+    public static void main(String[] args) {
+        int v;
+        double x;
+
+        v = 10;
+        x = 10.0;
+
+        System.out.println("Первоначальное значение v " + v);
+        System.out.println("Первоначальное значение x " + x);
+
+        System.out.println();
+
+        v = v / 4;
+        x = x / 4;
+
+        System.out.println("v после деления " + v);
+        System.out.println("x после деления " + x);
+    }
+}
